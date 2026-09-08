6fb9fb4446e98c4d4e1586cf7189fcc3cd6f83df
rohitbjayadev <rohitjdev2004@gmail.com>
2026-09-08 23:55:29 +0530

    Configure Next.js to allow Squarespace CDN images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.squarespace-cdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static1.squarespace.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
